d4aa836121bd3d74b22f7c2119c7042b854ba9d1
raman <raman@ramandekonda>
2022-03-09 23:09:34 +0530

    second repository

--- Git_Text/src/LandT/com/HelloWorld.java
+++ Git_Text/src/LandT/com/HelloWorld.java
@@ -3,7 +3,9 @@ package LandT.com;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("HEY SIMBA");
+		System.out.println("HEY RAMAN");
+		int a=10;
+		
 
 	}
 
